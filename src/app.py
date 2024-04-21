8e16ca3fed8bbac2602915e9c13e6e4f0431172c
PabloTSuarez <160293077+PabloTSuarez@users.noreply.github.com>
2024-04-21 15:21:58 +0000

    Ejercicio Terminado

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+#Defino librerias, variables de conexion y establezco la conexion
 import os
 import pandas as pd
 import seaborn as sns
@@ -11,6 +12,30 @@ load_dotenv()
 client_id = os.environ.get("CLIENT_ID")
 client_secret = os.environ.get("CLIENT_SECRET")
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,client_secret=client_secret))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,client_secret=client_secret,redirect_uri='https://localhost'))
 
 
+#Defino el string del artista a consultar + almaceno en variables la respuesta
+id_artista = '64tNsm6TnZe2zpcMVMOoHL'
+respuesta = sp.artist_top_tracks(id_artista)
+tracks = respuesta['tracks']
+
+
+#Defino el dataframe y lo muestro
+df_tracks = pd.DataFrame.from_records(tracks)
+df_tracks
+
+
+#Reordeno el DF y lo muestro
+df_tracks.sort_values(['popularity'],inplace=True)
+df_tracks
+
+#Muestro los primeros 3 despues de reordenarlos
+df_tracks.head(3)
+
+
+#Finalmente grafico, muestro y saco conclusiones
+import matplotlib.pyplot as plt
+
+plt.scatter(x=df_tracks['popularity'],y=df_tracks['duration_ms'])
+plt.show
